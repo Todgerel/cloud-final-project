7fc70030b9a4989f3f3af29a4006b25b610f510e
Todgerel <112902040+Todgerel@users.noreply.github.com>
2024-01-28 13:40:21 -0500

    Added frontend app.

---
+++ src/components/Skills.jsx
@@ -0,0 +1,55 @@
+import { useAppContext } from "../appContext";
+import { Element } from "react-scroll";
+// Data
+import { skillData, resume } from "../data";
+// Components
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { Title } from "./globalStyledComponents";
+
+export default function Skills() {
+  const { theme } = useAppContext();
+
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = '/resume.pdf';
+    link.download = 'resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  return (
+    <Element name={"Skills"} id="skills">
+      <section className="section">
+        <Container className="text-center">
+          <Title>
+            <h2>Skills</h2>
+            <div className="underline"></div>
+          </Title>
+          <Row className="mt-3 align-items-center">
+            {skillData.map((skills) => {
+              return (
+                <Col xs={4} key={skills.id} className="my-md-5">
+                  <figure>
+                    {skills.skill}
+                    <figcaption>{skills.name}</figcaption>
+                  </figure>
+                </Col>
+              );
+            })}
+          </Row>
+          {resume && (            
+            <Button
+              size="lg"
+              variant={theme === "light" ? "outline-dark" : "outline-light"}
+              className="mt-5"
+              onClick={handleDownload}
+            >
+              R&eacute;sum&eacute;
+            </Button>
+          )}
+        </Container>
+      </section>
+    </Element>
+  );
+}
